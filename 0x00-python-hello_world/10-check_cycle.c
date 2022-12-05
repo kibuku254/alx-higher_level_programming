7d6ce8dd7f42011a03bc0c88704850cc90b551bd
Kibuku <kibuku2@gmail.com>
2022-12-05 05:13:47 +0300

    hello world

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,28 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list is circular
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle
- * 0 if none
+ * check_cycle - a function to check if a linked list has a cycle
+ * @list: the head node
+ * Description: the function will return 1 if cycle else 0
+ * Return: 0 or 1
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *tortoise = list;
-	listint_t *hare = list;
+	listint_t *slow, *fast;
 
-	if (!list)
-		return (0);
+	slow = fast = list;
 
-	while (tortoise && hare && hare->next)
+	while (slow && fast && fast->next)
 	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-		if (slow == fast)
-			return (1);
-	}
+		slow = slow->next;
+		fast = fast->next->next;
 
+		if (fast == slow)
+		{
+			return (1);
+		}
+	}
 	return (0);
 }
+
